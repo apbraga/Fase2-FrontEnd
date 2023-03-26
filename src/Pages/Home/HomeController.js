e6e5d99b1efff32fd1e55afb8922c4649a38dbf5
Alex Braga <alexbraga101@gmail.com>
2023-03-26 17:07:28 -0300

    Add React Hooks

--- src/Pages/Home/HomeController.js
+++ src/Pages/Home/HomeController.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import HomeView from './HomeView'
 import HomeModel from './HomeModel'
-class HomeController extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            count: 0,
-        }; //initialization o state
-        this.homeModel = new HomeModel();//initialization o view
-        this.homeModel.getSomeInfo();
-        // Initialization o timeout
-        setInterval(() => {
-            this.setState({count: this.state.count + 1})
-        },1000);
-    }
-    render() {
-        console.log(" Count " + this.state.count);
-        return (
-            <HomeView count_info={this.state.count}>
-                <div> Teste Info</div>
-            </HomeView>
-        )
-    }
+
+const HomeController = () => {
+    const [count, setCount] = useState(0);
+    const homeModel = useRef(null);
+    useEffect(() => {
+        homeModel.current = new HomeModel();
+        homeModel.current.getSomeInfo();
+        const interval = setInterval(() => {
+            setCount(count + 1)
+        }, 1000);
+        return() =>{
+            clearInterval(interval);
+        };
+    },[])
+    console.log(" Count " + count);
+    return (
+        <HomeView count_info={count}>
+            <div> Teste Info </div>
+        </HomeView>
+    )
 }
 export default HomeController;
